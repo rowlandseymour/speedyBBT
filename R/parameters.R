b827cf4a556fe8e3af83d91886146a45b48a7847
stawitcc <c.c.stawitz@bham.ac.uk>
2026-07-15 11:20:13 +0100

    add helper extractor functions

---
+++ R/parameters.R
@@ -0,0 +1,54 @@
+#' This function helps the user extract relevant estimates from the quality parameters
+#' fit using a Bayesian Bradley-Terry model.
+#'
+#' @param model_output The mcmc object that contains the draws output from the BBT model
+#' @param indices_to_extract a single column or a vector of column indices to extract from the draws matrix
+#' @return a matrix with the selected indices, or the full matrix of draws
+lambda <- function(model_output, indices_to_extract = NULL) {
+  check_model(model_output, "lambda")
+
+  if (is.null(indices_to_extract)) {
+    return(model_output[, grep("lambda", varnames(model_output))])
+  } else {
+    return(model_output[, paste(
+      "lambda[",
+      indices_to_extract,
+      "]",
+      sep = ""
+    )])
+  }
+}
+
+
+#' This function helps the user extract relevant estimates from the variance parameter
+#' fit using a Bayesian Bradley-Terry model.
+#'
+#' @param model_output The mcmc object that contains the draws output from the BBT model
+#' @return the draws of the parameter
+#' @export
+parameter <- function(model_output, parameter_name) {
+  check_model(model_output, parameter_name)
+
+  return(model_output[, grep(parameter_name, varnames(model_output))])
+}
+
+#' internal helper
+#' @importFrom rlang abort
+check_model <- function(model_output, parameter_name) {
+  if (class(model_output) != "mcmc") {
+    rlang::abort(
+      "model_output must be an object of class mcmc for this function to work correctly."
+    )
+  }
+
+  if (length(grep(parameter_name, varnames(model_output)))) {
+    rlang::abort(
+      paste(
+        "This model object does not contain estimates of the ",
+        parameter_name,
+        " parameter. Please run a model using the `speedyBBTm` function."
+      ),
+      sep = ""
+    )
+  }
+}
