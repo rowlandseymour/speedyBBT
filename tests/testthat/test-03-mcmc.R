eebf5476580485175ca6c90e431bb585a43d1286
stawitcc <c.c.stawitz@bham.ac.uk>
2026-07-29 18:34:41 +0100

    add test for bbtm.no.formula

--- tests/testthat/test-03-mcmc.R
+++ tests/testthat/test-03-mcmc.R
@@ -71,7 +71,45 @@ test_that("BBTm produces results within tolerance", {
   )
 })
 
-test_that("BBTm produces results within tolerance when hyperparameter = FALSE", {
+test_that("BBTm.no.formula produces results within tolerance", {
+  # Construct covariance matrix
+  # Fit model
+  set.seed(332)
+  # Construct covariance matrix
+  expA <- expm::expm(forcedMarriage$adjacencyMatrix)
+  sigma <- diag(diag(expA)^-0.5) %*% expA %*% diag(diag(expA)^-0.5)
+
+  # Fit model
+  forcedMarriageModel <- BBTm(
+    outcome = rep(1, length(forcedMarriage$comparisons$win)),
+    player1 = forcedMarriage$comparisons$win,
+    player2 = forcedMarriage$comparisons$lost,
+    player.prior.var = sigma,
+    n.iter = 2000
+  )
+
+  lambda_draws <- forcedMarriageModel[
+    -c(1:100),
+    grep(
+      "lambda",
+      varnames(forcedMarriageModel)
+    )
+  ]
+  forcedMarriageModelMeans <- lambda_draws - colMeans(lambda_draws)
+
+  # Read in means
+  testMeansPath <- test_path("forcedMarriageModelMeans.csv")
+  testMeans <- read.csv(testMeansPath)
+
+  expect_equal(
+    sum(abs(testMeans - forcedMarriageModelMeans)) /
+      nrow(forcedMarriage$adjacencyMatrix),
+    0,
+    tolerance = 1e-1
+  )
+})
+
+test_that("BBTm.ties produces expected output from a single iteration", {
   # Construct covariance matrix
   # Fit model
   mod_run <- function() {
@@ -145,8 +183,7 @@ test_that("BBTm.no.formula produces results within tolerance", {
     n.iter = 1000
   )
 
-  lambda_means <- colMeans(forcedMarriageModel[
-    ,
+  lambda_means <- colMeans(forcedMarriageModel[,
     grep(
       "lambda",
       varnames(forcedMarriageModel)
