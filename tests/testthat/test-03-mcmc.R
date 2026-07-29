d91e609af0c002b64132ed6ec21b46fe6236e239
stawitcc <c.c.stawitz@bham.ac.uk>
2026-07-29 18:34:41 +0100

    add test for bbtm.no.formula

--- tests/testthat/test-03-mcmc.R
+++ tests/testthat/test-03-mcmc.R
@@ -76,8 +76,7 @@ test_that("BBTm.no.formula produces results within tolerance", {
     n.iter = 2000
   )
 
-  lambda_means <- colMeans(forcedMarriageModel[
-    ,
+  lambda_means <- colMeans(forcedMarriageModel[,
     grep(
       "lambda",
       varnames(forcedMarriageModel)
