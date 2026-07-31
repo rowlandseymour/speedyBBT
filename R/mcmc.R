8f7893d0a96bf94fc794c2c24de9b2ac9b4b4d81
stawitcc <c.c.stawitz@bham.ac.uk>
2026-07-31 12:45:11 +0100

    add a test for BBTm.no.formula (currently failing)

--- R/mcmc.R
+++ R/mcmc.R
@@ -490,6 +490,8 @@ BBTm.ties <- function(
 #'
 #' If `lambda.var` is omitted, it is set to N(0, 5^2).
 #'
+#' #' If `kappa.var` is omitted, it is set to N(0, 5^2), if `kappa.initial` is omitted
+#' it is set to 0.5.
 #'
 #' @return  A `coda::mcmc` object containing samples from the posterior distribution
 #'
@@ -561,6 +563,10 @@ BBTm.no.formula <- function(
       kappa <- 0.5
     } else {
       kappa <- kappa.initial
+    }
+
+    if (is.null(kappa.var)) {
+      kappa.var <- 5^2
     }
     kappa.precision <- 1 / kappa.var
     kappa.vector <- numeric(n.iter)
