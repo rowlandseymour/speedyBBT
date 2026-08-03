ff50777ac7154ab69f93b069ac7bdad4db09f783
stawitcc <c.c.stawitz@bham.ac.uk>
2026-08-03 17:50:57 +0100

    replace player with item

--- tests/longtests/test-mcmc-long.R
+++ tests/longtests/test-mcmc-long.R
@@ -8,9 +8,9 @@ test_that("BBTm.ties produces results within tolerance", {
   darTiedModel <- BBTm.ties(
     n.objects = n.objects,
     outcome = darEsSalaam$comparisons$outcome,
-    player1 = darEsSalaam$comparisons$subward1,
-    player2 = darEsSalaam$comparisons$subward2,
-    player.prior.var = sigma,
+    item1 = darEsSalaam$comparisons$subward1,
+    item2 = darEsSalaam$comparisons$subward2,
+    item.prior.var = sigma,
     hyperparameter = TRUE,
     rw.sd = 0.005,
     n.iter = 400
