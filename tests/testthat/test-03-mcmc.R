f33fb37259faffb9ffa2f264e613ea972f37633e
stawitcc <c.c.stawitz@bham.ac.uk>
2026-08-12 11:19:16 +0100

    Revert "replace player with item"
    This reverts commit 30b9f034db421b95ab527a8eb6f77f51fe876b28.

--- tests/testthat/test-03-mcmc.R
+++ tests/testthat/test-03-mcmc.R
@@ -7,9 +7,9 @@ test_that("speedyBBTm produces results within tolerance", {
   # Fit model
   forcedMarriageModel <- speedyBBTm(
     outcome = rep(1, length(forcedMarriage$comparisons$win)),
-    item1 = forcedMarriage$comparisons$win,
-    item2 = forcedMarriage$comparisons$lost,
-    item.prior.var = prior.var,
+    player1 = forcedMarriage$comparisons$win,
+    player2 = forcedMarriage$comparisons$lost,
+    player.prior.var = sigma,
     n.iter = 2000
   )
 
@@ -47,8 +47,8 @@ test_that("BBTm produces results within tolerance", {
   # Fit model
   wimbledonModel <- BBTm(
     outcome = wimbledon$matches$outcome,
-    item1 = wimbledon$matches$winner,
-    item2 = wimbledon$matches$loser,
+    player1 = wimbledon$matches$winner,
+    player2 = wimbledon$matches$loser,
     advantage = wimbledon$matches$secondWeek,
     formula = ~ rank + points,
     data = wimbledon$players,
@@ -82,9 +82,9 @@ test_that("BBTm.no.formula produces results within tolerance", {
   # Fit model
   forcedMarriageModel <- BBTm(
     outcome = rep(1, length(forcedMarriage$comparisons$win)),
-    item1 = forcedMarriage$comparisons$win,
-    item2 = forcedMarriage$comparisons$lost,
-    item.prior.var = prior.var,
+    player1 = forcedMarriage$comparisons$win,
+    player2 = forcedMarriage$comparisons$lost,
+    player.prior.var = sigma,
     n.iter = 2000
   )
 
