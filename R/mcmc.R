142d207bf55416c8f40c394c6bc3b616a2b8a8d5
stawitcc <c.c.stawitz@bham.ac.uk>
2026-09-09 10:17:22 +0100

    fix docs attempt by changing to \eqn

--- R/mcmc.R
+++ R/mcmc.R
@@ -482,8 +482,8 @@ BBTm.ties <- function(
 #' This function fits the Bradley-Terry model with a comparison specific effect.
 #' Each comparison can be assigned a real value to allow for a specific effect
 #' for the comparison, such as bias, ordering or home/away effect. The value of
-#' this effect is denoted $\kappa$. The function places a normal prior distribution
-#' on both kappa and the player quality parameters lambda.
+#' this effect is denoted \eqn{\kappa}. The function places a normal prior distribution
+#' on both kappa and the player quality parameters \eqn{\lambda}.
 #'
 #'
 #'
@@ -500,7 +500,7 @@ BBTm.ties <- function(
 #'  effect for each comparison.
 #' @param kappa.initial (optional) An initial value for the comparison specific
 #'  value kappa.
-#'  @param kappa.var (optional) The prior variance of the comparison specific
+#' @param kappa.var (optional) The prior variance of the comparison specific
 #'  value kappa.
 #' @param hyperparameter Boolean indicating if inference should be performed
 #'  for the prior variance hyperparameter. If TRUE the prior variance
