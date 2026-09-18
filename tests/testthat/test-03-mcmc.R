728b4c4beb1b9d853b63897286917ebe9a264e36
stawitcc <c.c.stawitz@bham.ac.uk>
2026-09-18 15:16:55 +0100

    adding single variant

--- tests/testthat/test-03-mcmc.R
+++ tests/testthat/test-03-mcmc.R
@@ -68,8 +68,7 @@ test_that("BBTm produces results within tolerance", {
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -98,8 +97,7 @@ test_that("BBTm produces results within tolerance when hyperparameter = FALSE", 
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -158,8 +156,7 @@ test_that("BBTm produces results within tolerance when hyperparameter = FALSE an
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -188,8 +185,7 @@ test_that("BBTm.no.formula without advantage and hyperparameter=FALSE produces r
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -220,8 +216,7 @@ test_that("BBTm.no.formula with advantage and hyperparameter=FALSE produces resu
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -252,8 +247,7 @@ test_that("BBTm.no.formula with advantage and hyperparameter=TRUE produces resul
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -290,8 +284,7 @@ test_that("BBTm.ties produces expected output from two iterations", {
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
 
@@ -331,7 +324,6 @@ test_that("BBTm.ties produces expected output from two iterations when hyperpara
   expect_snapshot_value(
     mod_run(),
     style = "serialize",
-    tolerance = 1e-1,
-    variant = Sys.info()[["sysname"]]
+    tolerance = 1e-1
   )
 })
